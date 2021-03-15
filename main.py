607ff39f8ba9b2635ee650f5e0db7224e2e6785b
pooopkaaa <60960322+pooopkaaa@users.noreply.github.com>
2021-03-15 18:29:55 +0300

    Добавил функцию группировки по категориям напитков

--- main.py
+++ main.py
@@ -2,14 +2,24 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
 import pandas
+import pprint
 
 BIRTH_YEAR = 1920
 
 def read_file():
-    excel_data_df = pandas.read_excel('wine.xlsx', sheet_name='Лист1')
+    excel_data_df = pandas.read_excel('wine2.xlsx', sheet_name='Лист1', na_values='nan', keep_default_na=False)
     return excel_data_df.to_dict(orient='records')
 
+def group_wine_cards(wine_cards):
+    wine_cards_groups = {}
+    for wine_card in wine_cards:
+        key = wine_card['Категория']
+        wine_cards_groups.setdefault(key, []).append(wine_card)
+    return wine_cards_groups
+
 wine_cards = read_file()
+wine_cards_groups = group_wine_cards(wine_cards)
+pprint.pprint(wine_cards_groups)
 
 env = Environment(
     loader=FileSystemLoader('.'),
