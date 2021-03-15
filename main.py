c5acd15be525c732c1863815e4149feaf7048c48
pooopkaaa <60960322+pooopkaaa@users.noreply.github.com>
2021-03-15 23:13:21 +0300

    Добавил сортировку при чтении файла excel

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from collections import defaultdict
 BIRTH_YEAR = 1920
 
 def read_file():
-    excel_data_df = pandas.read_excel('wine3.xlsx', sheet_name='Лист1', na_values='nan', keep_default_na=False)
-    return excel_data_df.to_dict(orient='records')
+    wine_data = pandas.read_excel('wine3.xlsx', sheet_name='Лист1', na_values='nan', keep_default_na=False).sort_values(by='Категория')
+    return wine_data.to_dict(orient='records')
 
 def group_wine_cards(wine_cards):
     wine_cards_groups = defaultdict(list)
@@ -36,7 +36,6 @@ rendered_page = template.render(
     delta_year=delta_year,
     wine_cards_groups=wine_cards_groups,
 )
-
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
 
