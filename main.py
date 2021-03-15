9917bc55f72ba175ea5f255b8093536aa903d0d4
pooopkaaa <60960322+pooopkaaa@users.noreply.github.com>
2021-03-15 13:25:22 +0300

    Исправил возраст винодельни

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import datetime
 
+BIRTH_YEAR = 1920
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+current_year = datetime.date.today().year
+delta_year = str(current_year - BIRTH_YEAR)
+
+rendered_page = template.render(
+    delta_year = delta_year,
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
