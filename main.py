da3477ee744bdc4ebd12b50b5c78548b8d168c92
pooopkaaa <60960322+pooopkaaa@users.noreply.github.com>
2021-03-17 13:50:49 +0300

    Переименовал переменные для лучшего восприятия

--- main.py
+++ main.py
@@ -25,11 +25,11 @@ def read_file(filepath):
 
 
 def group_wine_cards(wine_cards):
-    wine_cards_groups = defaultdict(list)
+    grouped_wine_cards = defaultdict(list)
     for wine_card in wine_cards:
         key = wine_card['Категория']
-        wine_cards_groups[key].append(wine_card)
-    return wine_cards_groups
+        grouped_wine_cards[key].append(wine_card)
+    return grouped_wine_cards
 
 
 def get_company_age():
@@ -54,12 +54,12 @@ def main():
     wine_cards_filepath = get_wine_cards_xlsx_filepath()
     company_age = get_company_age()
     wine_cards = read_file(wine_cards_filepath)
-    wine_cards_groups = group_wine_cards(wine_cards)
+    grouped_wine_cards = group_wine_cards(wine_cards)
 
     template = env.get_template('template.html')
     rendered_page = template.render(
         company_age=company_age,
-        wine_cards_groups=wine_cards_groups,
+        grouped_wine_cards=grouped_wine_cards,
     )
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
